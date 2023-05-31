a29c52bd38af1dbb92083ea0e0e813d923257458
j0131n <dev.jcantoria@gmail.com>
2023-05-31 23:14:57 +0800

    add index.js

---
+++ index.js
@@ -0,0 +1,41 @@
+function leftPad(str, len, char) {
+	return new Array(len - str.length).fill(!char && char !== 0 ? ' ' : char).join('') + str;
+}
+
+function leftPad2(str, len, ch) {
+	str = String(str);
+  var i = -1;
+  if (!ch && ch !== 0) ch = ' ';
+  
+  len = len - str.length;
+  while(++i < len) {
+  	str = ch + str;
+  }
+  return str;
+}
+
+function runPerf(fn, count, ...args) {
+	const start = performance.now();
+  
+  for(let i = 0; i < count; i++) {
+  	fn.apply(null, args);
+  }
+  
+  return performance.now() - start;
+}
+
+/* sample benchmark */
+[10, 100, 1000, 10000].forEach(x => {
+	console.log('run leftPad', runPerf(leftPad, x, 'foo', x));
+});
+
+[10, 100, 1000, 10000].forEach(x => {
+	console.log('run leftPad2', runPerf(leftPad2, x, 'foo', x));
+});
+
+/* isArray helper */
+function isArray(str) {
+	return toString.call(str) == '[object Array]';
+}
+
+console.log('yep', isArray([]) );
